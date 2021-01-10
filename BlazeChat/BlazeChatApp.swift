7ef3994a0bf5fe590930945ba024fc18cbc67a0a
bejitono <stdemicheli@protonmail.com>
2021-01-10 11:06:40 +0700

    Sets up initial project.

---
+++ BlazeChat/BlazeChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlazeChatApp.swift
+//  BlazeChat
+//
+//  Created by De MicheliStefano on 10.01.21.
+//
+
+import SwiftUI
+
+@main
+struct BlazeChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
