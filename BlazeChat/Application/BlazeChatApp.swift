85e56c7e4a1a60a1ccca9b1795359f94e829f0f9
bejitono <stdemicheli@protonmail.com>
2021-01-11 20:56:43 +0700

    Sets up initial project structure.

--- BlazeChat/Application/BlazeChatApp.swift
+++ BlazeChat/Application/BlazeChatApp.swift
@@ -5,13 +5,26 @@
 //  Created by De MicheliStefano on 10.01.21.
 //
 
+import LightningKit
 import SwiftUI
 
 @main
 struct BlazeChatApp: App {
+    
+    init() {
+        setupLnd()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+private extension BlazeChatApp {
+    
+    func setupLnd() {
+        LKChannel.shared.start()
     }
 }
--- BlazeChat/ContentView.swift
+++ BlazeChat/ContentView.swift
@@ -5,14 +5,9 @@
 //  Created by De MicheliStefano on 10.01.21.
 //
 
-import LightningKit
 import SwiftUI
 
 struct ContentView: View {
-    
-    init() {
-        LKChannel.shared.start()
-    }
     
     var body: some View {
         Text("Hello, world!")
