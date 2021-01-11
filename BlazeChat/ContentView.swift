44ed5cf210310323a7340c0f46ff2c08e98778a1
bejitono <stdemicheli@protonmail.com>
2021-01-11 20:37:43 +0700

    Adds LightningKit dependency.

--- BlazeChat/ContentView.swift
+++ BlazeChat/ContentView.swift
@@ -5,9 +5,15 @@
 //  Created by De MicheliStefano on 10.01.21.
 //
 
+import LightningKit
 import SwiftUI
 
 struct ContentView: View {
+    
+    init() {
+        LKChannel.shared.start()
+    }
+    
     var body: some View {
         Text("Hello, world!")
             .padding()
