59935132c035d37f928c3ca736d74e5e4b160eac
bejitono <stdemicheli@protonmail.com>
2021-01-16 22:37:04 +0700

    Implements seed view controller.

---
+++ BlazeChat/Modules/Registration/Seed/SeedViewController.swift
@@ -0,0 +1,77 @@
+//
+//  SeedViewController.swift
+//  BlazeChat
+//
+//  Created by Stefano on 16.01.21.
+//
+
+import LightningKitUI
+import UIKit
+
+final class SeedViewController: UIViewController {
+    
+    private let loadingView = LoadingViewController()
+    private let viewModel: SeedViewModelInput
+    
+    init(viewModel: SeedViewModelInput) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupLoadingView()
+        viewModel.generateSeed()
+    }
+}
+
+// MARK: - SeedViewModelOutput
+
+extension SeedViewController: SeedViewModelOutput {
+    
+    func didGenerate(seed: [String]) {
+        setupSeedView(with: seed)
+    }
+    
+    func didThrow(error: String) {
+        // Handle error
+    }
+}
+
+// MARK: - SeedViewControllerDelegate
+
+extension SeedViewController: SeedViewControllerDelegate {
+    
+    func didTapDone() {
+        viewModel.confirmSeed()
+    }
+}
+
+// MARK: - View Setup
+
+private extension SeedViewController {
+    
+    func setupLoadingView() {
+        view.backgroundColor = Theme.color.primaryBackground
+        loadingView.modalPresentationStyle = .fullScreen
+        navigationController?.present(loadingView, animated: true)
+    }
+    
+    func setupSeedView(with seed: [String]) {
+        let seedView = LightningKitUI.SeedViewController(seed: seed)
+        seedView.delegate = self
+        seedView.view.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(seedView.view)
+        NSLayoutConstraint.activate([
+            seedView.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            seedView.view.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            seedView.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            seedView.view.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+        loadingView.dismiss(animated: true)
+    }
+}
